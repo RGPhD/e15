41f3689eaf116c6264562b578a3ae16f4a3e1eb6
Robelyn Garcia <rog814@g.harvard.edu>
2020-02-06 18:23:51 -0700

    Initialized P1.

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: *Robelyn Garcia*
++ Production URL: <http://e15p1.robelyngarcia.me>
+
+## Outside resources
++ *List of outside resources go here.*
+
+## Notes for instructor
++ *Notes to refer to while grading.*
---
+++ p1/index-view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>e15 Project 1</title>
+    <meta charset='utf-8'>
+</head>
+
+<body>
+
+    <h1>e15 Project 1</h1>
+
+    <p>
+       Robelyn Garcia 
+    </p>
+
+<hr>
+
+<footer>
+CSCI E-15
+</footer>
+
+</body>
+</html>
