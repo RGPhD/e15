bc49e6e95c7afdfa88cfc770178445e9aa239618
Robelyn Garcia <rog814@g.harvard.edu>
2020-02-06 17:40:13 -0700

    Practice PHPiggy Bank App.

---
+++ practice/index-view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>PHPiggy Bank</title>
+    <meta charset='utf-8'>
+</head>
+
+<body>
+
+    <img alt='PHPiggy Bank Logo' src='https://s3.amazonaws.com/making-the-internet/php-piggy-bank-logo@2x.png' style='width:202px;'>
+
+    <p>
+        You have $<?php echo $total; ?> in your piggy bank.
+    </p>
+<hr>
+<footer style='text-align:center;'>
+Practice Application for CSCI E-15
+</footer>
+</body>
+</html>
