d6fbab8da7d855c4728d37377ff9915bc5c89bff
Robelyn Garcia <rog814@g.harvard.edu>
2020-02-10 13:23:19 -0700

    P1 logic and style.

--- p1/index-view.php
+++ p1/index-view.php
@@ -1,25 +1,41 @@
 <!DOCTYPE html>
 <html lang='en'>
+
 <head>
-    <title>e15 Project 1</title>
-    <meta charset='utf-8'>
+  <title>String Word Processor: E-15 Project 1</title>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+  <!-- External CSS file providing Separation of Concerns (SoC). -->
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+  <h1>PHP String Processor: E-15 Project 1</h1>
+  <h2>by Robelyn Garcia</>
 
-    <h1>e15 Project 1</h1>
-
-    <p> Robelyn Garcia </p>
-
-    <!-- Professor Sample -->
     <p><?php echo $result1; ?></p>
     <p><?php echo $result2; ?></p>
 
-<hr>
+    <p><?php echo $result3; ?></p>
+    <p><?php echo $result4; ?></p>
 
-<footer>
-CSCI E-15
-</footer>
+    <p><?php echo $result5; ?></p>
+    <p><?php echo $result6; ?></p>
+
+    <p><?php echo $result7; ?></p>
+    <p><?php echo $result8; ?></p>
+
+      <br>
+    <footer>
+      <br>
+      <a href="https://hesweb.dev/e15" target="_blank"
+        title="Click or Tap to visit Harvard Extension School: Web Server Frameworks with Laravel/PHP">DGMD E-15: Web
+        Server Frameworks with Laravel/PHP
+      </a>
+
+      <p><a href="https://www.extension.harvard.edu" target="_blank"
+          title="Click to visit Harvard Extension School">Harvard University Extension School</a></p>
+    </footer>
 
 </body>
-</html>
--- p1/index.php
+++ p1/index.php
@@ -1,15 +1,28 @@
 <?php
-
-# (Required) Is palindrome? same forward and back
+/* Multi-line comment.  
+This is the controller file.
+*/
 function isPalindrome($inputString)
 {
+    if (strrev($inputString)==($inputString)) {
+        return 'This is a Palindrome';
+    } else {
+        return 'This is not a Palindrome';
+    }
 }
 
-# 2. (Required) Vowel count
-# Indicates a count of how many vowels (aeiou) 
-# are in the input string. Case insensitive.
+function vowelCount($inputString)
+{
 
-# Susan Buck sample code
+preg_match_all('/[aeiou]/i', $inputString, $vowels);
+    return count($vowels[0]);
+}
+
+function upperCase($inputString)
+{
+   return strtoupper($inputString);
+}
+
 function isBigWord($inputString)
 {
     if (strlen($inputString) > 7) {
@@ -21,6 +34,15 @@ function isBigWord($inputString)
 
 $result1 = isBigWord('cat');
 $result2 = isBigWord('mississippi');
+
+$result3 = isPalindrome('siamese');
+$result4 = isPalindrome('madam');
+
+$result5 = vowelCount('basketball');
+$result6 = vowelCount('arizona');
+
+$result7 = upperCase('senior games');
+$result8 = upperCase('scottsdale');
 
 # Can remove the closing php tag if only php is in the file.
 require 'index-view.php';
