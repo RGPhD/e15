5bc301f660ed65ab9fe04408b3be87fbb4fe2f29
Robelyn Garcia <rog814@g.harvard.edu>
2020-04-03 19:17:58 -0700

    Created database with myphpadmin and debug/config.

--- bookmark/routes/web.php
+++ bookmark/routes/web.php
@@ -4,6 +4,32 @@ Route::get('/example2', function () {
     dump(Arr::sort(['a', 'z', 'g']));
 });
 
+Route::get('/debug', function () {
+
+    $debug = [
+        'Environment' => App::environment(),
+    ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
+});
+
 # Example route used to demonstrate error pages
 Route::get('/example', function () {
     $foo = [1,2,3];
