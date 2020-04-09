af7bf5028fe120ec51ebf01cc6f8e1f361294505
Robelyn Garcia <rog814@g.harvard.edu>
2020-04-08 22:30:59 -0700

    Updating p3 configs and routes.

--- p3/routes/web.php
+++ p3/routes/web.php
@@ -18,3 +18,29 @@ Route::get('/', function () {
     // return view('welcome');
     // change this to  p3 view
 });
+
+Route::get('/debug', function () {
+
+    $debug = [
+        'Environment' => App::environment(),
+    ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
+});
