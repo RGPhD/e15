b6b08d549dc729780834e355c896f03274d0f18b
Robelyn Garcia <rog814@g.harvard.edu>
2020-04-10 22:41:30 -0700

    updating bookstable

--- bookmark/database/seeds/DatabaseSeeder.php
+++ bookmark/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       ///$this->call(UsersTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
+       //remove backslashes if I create this seeder//$this->call(UsersTableSeeder::class);
     }
 }
