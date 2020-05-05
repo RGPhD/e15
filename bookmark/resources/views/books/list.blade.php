22d2ab768a5ca27e105d84fbd9d31bb37e426d06
Robelyn Garcia <rog814@g.harvard.edu>
2020-05-05 03:45:11 -0700

    Bookmark List updates.

--- bookmark/resources/views/books/list.blade.php
+++ bookmark/resources/views/books/list.blade.php
@@ -11,5 +11,7 @@
 @section('content')
     <h1>Your List</h1>
 
-    <em>This page will display all the books on your list...</em>
+    <h2>Harry Potter and the Chamber of Secrets</h2>
+    <em>By J.K. Rowling (1998)</em>
+    <p>Harry Potter and the Chamber of Secrets is a fantasy novel written by British author J. K. Rowling and the second novel in the Harry Potter series. The plot follows Harry’s second year at Hogwarts School of Witchcraft and Wizardry, during which a series of messages on the walls of the school’s corridors warn that the “Chamber of Secrets” has been opened and that the “heir of Slytherin” would kill all pupils who do not come from all-magical families. These threats are found after attacks that leave residents of the school petrified. Throughout the year, Harry and his friends Ron and Hermione investigate the attacks.</p>
 @endsection
