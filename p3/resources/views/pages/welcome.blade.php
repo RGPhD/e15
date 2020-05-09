149068ab2cedb9bab5b9db34760940f853401aa2
Robelyn Garcia <rog814@g.harvard.edu>
2020-05-09 06:25:37 -0700

    P3 updates.

--- p3/resources/views/pages/welcome.blade.php
+++ p3/resources/views/pages/welcome.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.master')
 
-<!--@section('head')
-<link href='/css/pages/welcome.css' rel='stylesheet'>
-@endsection-->
-
 @section('content')
 <p>We are Coronavirus Websites, an online website repository that will allow you to lookup, save your favorites, and "VIEW AND REVIEW" Coronavirus Health and or Financial related websites.  This website is not meant to be a replacement for medical treatment.  We are here to help you create a list of organizations that you and your household can utilize in the event you need access to health information and or financial information to help protect yourself during this pandemic.</p>
 
